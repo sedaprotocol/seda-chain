b389dfee6387be2466dc6e3546e53360d01b51aa
Hyoung-yoon Kim <hykim.home@gmail.com>
2023-10-13 18:00:37 -0400

    fix: init cmds bug fixes and refactor

--- cmd/seda-chaind/cmd/init.go
+++ cmd/seda-chaind/cmd/init.go
@@ -11,30 +11,13 @@ import (
 	"github.com/spf13/cobra"
 
 	cfg "github.com/cometbft/cometbft/config"
-	"github.com/cometbft/cometbft/libs/cli"
 	"github.com/cometbft/cometbft/types"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/input"
-	"github.com/cosmos/cosmos-sdk/server"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/x/genutil"
 	"github.com/cosmos/go-bip39"
 
-	"github.com/sedaprotocol/seda-chain/app/params"
 	"github.com/sedaprotocol/seda-chain/cmd/seda-chaind/utils"
-)
-
-const (
-	// FlagOverwrite defines a flag to overwrite an existing genesis JSON file.
-	FlagOverwrite = "overwrite"
-	// FlagSeed defines a flag to initialize the private validator key from a specific seed.
-	FlagRecover = "recover"
-
-	// Default things
-	ChainID = "sedachain"
 )
 
 type printInfo struct {
@@ -81,196 +64,48 @@ func validateOrGenerateMnemonic(recover bool, cmd *cobra.Command) (string, error
 	return mnemonic, nil
 }
 
-// preserve old logic for if we want to create a new network
-// though its slightly modified to set default settings.
-func newNetworkCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "new [moniker]",
-		Short: "Initialize private validator, p2p, genesis, and application configuration files",
-		Long:  `Initialize validators's and node's configuration files.`,
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd)
-			cdc := clientCtx.Codec
+// downloadAndApplyNetworkConfig() downloads network files from seda-networks
+// repo. Then it validates the genesis file and writes the seed list and given
+// moniker to the config file.
+func downloadAndApplyNetworkConfig(network, moniker string, config *cfg.Config) (chainID, seeds string, err error) {
+	configDir := filepath.Join(config.RootDir, "config")
 
-			serverCtx := server.GetServerContextFromCmd(cmd)
-			config := serverCtx.Config
-			config.SetRoot(clientCtx.HomeDir)
-
-			recover, _ := cmd.Flags().GetBool(FlagRecover)
-			mnemonic, err := validateOrGenerateMnemonic(recover, cmd)
-			if err != nil {
-				return err
-			}
-
-			// Get initial height
-			initHeight, _ := cmd.Flags().GetInt64(flags.FlagInitHeight)
-			if initHeight < 1 {
-				initHeight = 1
-			}
-
-			nodeID, _, err := genutil.InitializeNodeValidatorFilesFromMnemonic(config, mnemonic)
-			if err != nil {
-				return err
-			}
-
-			config.Moniker = args[0]
-
-			overwrite, _ := cmd.Flags().GetBool(FlagOverwrite)
-			genFile := config.GenesisFile()
-			// use os.Stat to check if the file exists
-			_, err = os.Stat(genFile)
-			if !overwrite && !os.IsNotExist(err) {
-				return fmt.Errorf("genesis.json file already exists: %v", genFile)
-			}
-
-			sdk.DefaultBondDenom = params.DefaultBondDenom
-			appGenState := mbm.DefaultGenesis(cdc)
-
-			appState, err := json.MarshalIndent(appGenState, "", " ")
-			if err != nil {
-				return errors.Wrap(err, "Failed to marshal default genesis state")
-			}
-
-			genDoc := &types.GenesisDoc{}
-			if _, err := os.Stat(genFile); err != nil {
-				if !os.IsNotExist(err) {
-					return err
-				}
-			} else {
-				genDoc, err = types.GenesisDocFromFile(genFile)
-				if err != nil {
-					return errors.Wrap(err, "Failed to read genesis doc from file")
-				}
-			}
-
-			genDoc.ChainID = ChainID
-			genDoc.Validators = nil
-			genDoc.AppState = appState
-			genDoc.InitialHeight = initHeight
-
-			if err = genutil.ExportGenesisFile(genDoc, genFile); err != nil {
-				return errors.Wrap(err, "Failed to export genesis file")
-			}
-			toPrint := newPrintInfo(config.Moniker, ChainID, nodeID, "")
-			cfg.WriteConfigFile(filepath.Join(config.RootDir, "config", "config.toml"), config)
-			return displayInfo(toPrint)
-		},
+	// use os.Stat to check if the file exists
+	_, err = os.Stat(config.GenesisFile())
+	if !os.IsNotExist(err) {
+		return "", "", fmt.Errorf("genesis.json file already exists: %v", config.GenesisFile())
 	}
 
-	cmd.Flags().String(cli.HomeFlag, defaultNodeHome, "node's home directory")
-	cmd.Flags().BoolP(FlagOverwrite, "o", false, "overwrite the genesis.json file")
-	cmd.Flags().Bool(FlagRecover, false, "provide seed phrase to recover existing key instead of creating")
-	cmd.Flags().Int64(flags.FlagInitHeight, 1, "specify the initial block height at genesis")
-
-	return cmd
-}
-
-func joinNetwork(network, configDir, genesisFilePath, seedsFilePath, configFilePath, mnemonic string, config *cfg.Config) error {
-	err := utils.DownloadGitFiles(network, configDir)
+	// download files from seda-networks repo
+	err = utils.DownloadGitFiles(network, configDir)
 	if err != nil {
-		return errors.Wrapf(err, "failed to download network `%s` genesis files", network)
+		return "", "", errors.Wrapf(err, "failed to download files for network `%s`", network)
 	}
 
-	bytes, err := os.ReadFile(genesisFilePath)
+	// check genesis file
+	genFile := config.GenesisFile()
+	jsonBlob, err := os.ReadFile(genFile)
 	if err != nil {
-		return err
+		return "", "", err
 	}
-	var genesisExistingState map[string]json.RawMessage
-	err = json.Unmarshal(bytes, &genesisExistingState)
+	genDoc, err := types.GenesisDocFromJSON(jsonBlob)
 	if err != nil {
-		return err
+		return "", "", errors.Wrapf(err, "error reading GenesisDoc at %s", genFile)
 	}
-	_, err = json.MarshalIndent(genesisExistingState, "", " ")
-	if err != nil {
-		return errors.Wrapf(err, "Failed to marshal network `%s` genesis state", network)
-	}
+	chainID = genDoc.ChainID
 
-	// obtain seeds from seeds file and write to config file
-	seedsBytes, err := os.ReadFile(seedsFilePath)
+	// obtain seeds from seeds file, if exists, and write to config file
+	seedsBytes, err := os.ReadFile(filepath.Join(configDir, "seeds.txt"))
 	if err != nil {
-		return err
+		if !os.IsNotExist(err) {
+			return "", "", err
+		}
 	}
-	seeds := string(seedsBytes)
+	seeds = string(seedsBytes)
 
 	config.P2P.Seeds = seeds
-	cfg.WriteConfigFile(configFilePath, config)
+	config.Moniker = moniker
+	cfg.WriteConfigFile(filepath.Join(configDir, "config.toml"), config)
 
-	nodeID, _, err := genutil.InitializeNodeValidatorFilesFromMnemonic(config, mnemonic)
-	if err != nil {
-		return err
-	}
-
-	toPrint := newPrintInfo(config.Moniker, ChainID, nodeID, seeds)
-	return displayInfo(toPrint)
-}
-
-func existingNetworkComand(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "join [moniker]",
-		Short: "Grabs an existing network genesis configuration.",
-		Long:  `Initialize validators's and node's configuration files from an existing configuration.`,
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			serverCtx := server.GetServerContextFromCmd(cmd)
-			config := serverCtx.Config
-
-			recover, _ := cmd.Flags().GetBool(FlagRecover)
-			mnemonic, err := validateOrGenerateMnemonic(recover, cmd)
-			if err != nil {
-				return err
-			}
-
-			// get the value of the network flag
-			network, _ := cmd.Flags().GetString("network")
-			overwrite, _ := cmd.Flags().GetBool(FlagOverwrite)
-			configDir := filepath.Join(config.RootDir, "config")
-			genesisFilePath := filepath.Join(configDir, "genesis.json")
-			seedsFilePath := filepath.Join(configDir, "seeds.txt")
-			configFilePath := filepath.Join(configDir, "config.toml")
-
-			// use os.Stat to check if the file exists
-			_, err = os.Stat(genesisFilePath)
-			if !overwrite && !os.IsNotExist(err) {
-				return fmt.Errorf("genesis.json file already exists: %v", genesisFilePath)
-			}
-
-			// If we are overwriting the genesis make sure to remove gentx folder
-			// this is in case they are switching to a different network
-			if overwrite {
-				gentxDir := filepath.Join(configDir, "gentx")
-				err = os.RemoveAll(gentxDir)
-				if err != nil {
-					return err
-				}
-			}
-
-			// TODO should turn the insides here into a function for when we have more than one network
-			if network == "devnet" || network == "testnet" || network == "localnet" {
-				return joinNetwork(network, configDir, genesisFilePath, seedsFilePath, configFilePath, mnemonic, config)
-			}
-			return fmt.Errorf("unsupported network type: %s", network)
-		},
-	}
-
-	cmd.Flags().Bool(FlagRecover, false, "provide seed phrase to recover existing key instead of creating")
-	cmd.Flags().BoolP(FlagOverwrite, "o", false, "overwrite the genesis.json file")
-	cmd.Flags().StringP("network", "n", "devnet", "Specify the type of network to initialize (e.g., 'mainnet', 'testnet', 'devnet')")
-
-	return cmd
-}
-
-// InitCmd returns a command that initializes all files needed for Tendermint
-// and the respective application.
-func InitCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "init <new | join> [moniker]",
-		Short: "Initialize private validator, p2p, genesis, and application configuration files",
-		Long:  `Initialize validators's and node's configuration files.`,
-		Args:  cobra.ExactArgs(1),
-	}
-
-	cmd.AddCommand(newNetworkCmd(mbm, defaultNodeHome))
-	cmd.AddCommand(existingNetworkComand(mbm, defaultNodeHome))
-	return cmd
+	return chainID, seeds, nil
 }
---
+++ cmd/seda-chaind/cmd/init_cmds.go
@@ -0,0 +1,188 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	cfg "github.com/cometbft/cometbft/config"
+	"github.com/cometbft/cometbft/libs/cli"
+	"github.com/cometbft/cometbft/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+
+	"github.com/sedaprotocol/seda-chain/app/params"
+)
+
+const (
+	// FlagOverwrite defines a flag to overwrite an existing genesis JSON file.
+	FlagOverwrite = "overwrite"
+	// FlagSeed defines a flag to initialize the private validator key from a specific seed.
+	FlagRecover = "recover"
+	// FlagNetwork defines a flag to indicate which network to connect to.
+	FlagNetwork = "network"
+
+	// Default things
+	defaultChainID = "sedachain"
+)
+
+// add initialization commands
+func InitCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "init",
+		Short: "Initialize private validator, p2p, genesis, and application configuration files",
+	}
+	cmd.AddCommand(newNetworkCmd(mbm, defaultNodeHome))
+	cmd.AddCommand(joinNetworkCommand(mbm, defaultNodeHome))
+	return cmd
+}
+
+// preserve old logic for if we want to create a new network
+// though its slightly modified to set default settings.
+func newNetworkCmd(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "new [moniker]",
+		Short: "Initialize private validator, p2p, genesis, and application configuration files",
+		Long:  `Initialize validator and node configuration files for a new network.`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			cdc := clientCtx.Codec
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			config := serverCtx.Config
+			config.SetRoot(clientCtx.HomeDir)
+
+			recover, _ := cmd.Flags().GetBool(FlagRecover)
+			mnemonic, err := validateOrGenerateMnemonic(recover, cmd)
+			if err != nil {
+				return err
+			}
+
+			// get chain ID
+			chainID, _ := cmd.Flags().GetString(flags.FlagChainID)
+
+			// get initial height
+			initHeight, _ := cmd.Flags().GetInt64(flags.FlagInitHeight)
+			if initHeight < 1 {
+				initHeight = 1
+			}
+
+			// initialize node
+			nodeID, _, err := genutil.InitializeNodeValidatorFilesFromMnemonic(config, mnemonic)
+			if err != nil {
+				return err
+			}
+
+			// write to config file and genesis file and display info
+			config.Moniker = args[0]
+
+			overwrite, _ := cmd.Flags().GetBool(FlagOverwrite)
+			genFile := config.GenesisFile()
+			// use os.Stat to check if the file exists
+			_, err = os.Stat(genFile)
+			if !overwrite && !os.IsNotExist(err) {
+				return fmt.Errorf("genesis.json file already exists: %v", genFile)
+			}
+
+			sdk.DefaultBondDenom = params.DefaultBondDenom
+			appGenState := mbm.DefaultGenesis(cdc)
+
+			appState, err := json.MarshalIndent(appGenState, "", " ")
+			if err != nil {
+				return errors.Wrap(err, "Failed to marshal default genesis state")
+			}
+
+			genDoc := &types.GenesisDoc{}
+			if _, err := os.Stat(genFile); err != nil {
+				if !os.IsNotExist(err) {
+					return err
+				}
+			} else {
+				genDoc, err = types.GenesisDocFromFile(genFile)
+				if err != nil {
+					return errors.Wrap(err, "Failed to read genesis doc from file")
+				}
+			}
+
+			genDoc.ChainID = chainID
+			genDoc.Validators = nil
+			genDoc.AppState = appState
+			genDoc.InitialHeight = initHeight
+
+			if err = genutil.ExportGenesisFile(genDoc, genFile); err != nil {
+				return errors.Wrap(err, "Failed to export genesis file")
+			}
+			toPrint := newPrintInfo(config.Moniker, chainID, nodeID, "")
+			cfg.WriteConfigFile(filepath.Join(config.RootDir, "config", "config.toml"), config)
+			return displayInfo(toPrint)
+		},
+	}
+
+	cmd.Flags().String(cli.HomeFlag, defaultNodeHome, "node's home directory")
+	cmd.Flags().BoolP(FlagOverwrite, "o", false, "overwrite the genesis.json file")
+	cmd.Flags().Bool(FlagRecover, false, "provide seed phrase to recover existing key instead of creating")
+	cmd.Flags().String(flags.FlagChainID, defaultChainID, "genesis file chain-id")
+	cmd.Flags().Int64(flags.FlagInitHeight, 1, "specify the initial block height at genesis")
+
+	return cmd
+}
+
+func joinNetworkCommand(mbm module.BasicManager, defaultNodeHome string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "join [moniker] [network]",
+		Short: "Grabs an existing network configuration and initializes node based on it",
+		Long:  `Initialize validator and node configuration files for an existing network.`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			config := serverCtx.Config
+			config.SetRoot(clientCtx.HomeDir)
+
+			recover, _ := cmd.Flags().GetBool(FlagRecover)
+			mnemonic, err := validateOrGenerateMnemonic(recover, cmd)
+			if err != nil {
+				return err
+			}
+
+			network, _ := cmd.Flags().GetString(FlagNetwork)
+			var seeds, chainID string
+			if network == "mainnet" || network == "devnet" || network == "testnet" || network == "localnet" {
+				chainID, seeds, err = downloadAndApplyNetworkConfig(network, args[0], config)
+				if err != nil {
+					return err
+				}
+			} else {
+				return fmt.Errorf("unsupported network type: %s", network)
+			}
+
+			// initialize the node
+			nodeID, _, err := genutil.InitializeNodeValidatorFilesFromMnemonic(config, mnemonic)
+			if err != nil {
+				return err
+			}
+
+			// genesis and config files already written - display info
+			toPrint := newPrintInfo(config.Moniker, chainID, nodeID, seeds)
+			return displayInfo(toPrint)
+		},
+	}
+
+	cmd.Flags().Bool(FlagRecover, false, "provide seed phrase to recover existing key instead of creating")
+	cmd.Flags().StringP(FlagNetwork, "n", "devnet", "specify the name of network to initialize (e.g., 'mainnet', 'testnet', 'devnet', 'localnet')")
+	err := cmd.MarkFlagRequired(FlagNetwork)
+	if err != nil {
+		panic(err)
+	}
+
+	return cmd
+}
