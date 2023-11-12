0f3fb2414bddf406790e405a6ecb68e6d3b931e9
Hyoung-yoon Kim <hykim.home@gmail.com>
2023-11-12 12:21:57 +0000

    chore: remove unnecessary log

--- x/randomness/keeper/abci.go
+++ x/randomness/keeper/abci.go
@@ -15,7 +15,6 @@ import (
 
 func (k *Keeper) EndBlocker(ctx sdk.Context) []abci.ValidatorUpdate {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyEndBlocker)
-	k.Logger(ctx).Info("adding app hash %s", string(ctx.BlockHeader().AppHash))
 	k.SetSeed(ctx, strings.ToUpper(hex.EncodeToString(ctx.BlockHeader().AppHash)))
 	return nil
 }
