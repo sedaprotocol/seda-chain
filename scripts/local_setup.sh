71c388f09b5bc0684a0fd9f9961be88ce95d31ad
mennatabuelnaga <mennat.abuelnaga@gmail.com>
2023-12-06 16:34:56 +0200

    fix: add chain-id to local_setup.sh

--- scripts/local_setup.sh
+++ scripts/local_setup.sh
@@ -12,7 +12,7 @@ BIN=./build/seda-chaind
 
 $BIN tendermint unsafe-reset-all
 rm -rf ~/.seda-chain
-$BIN init new node0
+$BIN init new node0 --chain-id "sedachain"
 
 cat $HOME/.seda-chain/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="30s"' > $HOME/.seda-chain/config/tmp_genesis.json && mv $HOME/.seda-chain/config/tmp_genesis.json $HOME/.seda-chain/config/genesis.json
 cat $HOME/.seda-chain/config/genesis.json | jq '.app_state["gov"]["params"]["voting_period"]="30s"' > $HOME/.seda-chain/config/tmp_genesis.json && mv $HOME/.seda-chain/config/tmp_genesis.json $HOME/.seda-chain/config/genesis.json
@@ -21,7 +21,7 @@ cat $HOME/.seda-chain/config/genesis.json | jq '.app_state["gov"]["params"]["exp
 $BIN keys add satoshi --keyring-backend test
 ADDR=$($BIN keys show satoshi --keyring-backend test -a)
 $BIN add-genesis-account $ADDR 100000000000000000seda --keyring-backend test
-$BIN gentx satoshi 10000000000000000seda --keyring-backend test
+$BIN gentx satoshi 10000000000000000seda --keyring-backend test --chain-id "sedachain"
 
 $BIN keys add acc1 --keyring-backend test
 ADDR=$($BIN keys show acc1 --keyring-backend test -a)
