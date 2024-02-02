e409bbb4b523dea163b4d92036f2dab8ecc77c43
Jay Jie <jyz1992@hotmail.com>
2024-02-01 22:14:26 -0800

    fix: validator vrf key prefix

--- x/randomness/types/keys.go
+++ x/randomness/types/keys.go
@@ -1,7 +1,5 @@
 package types
 
-import "cosmossdk.io/collections"
-
 const (
 	// ModuleName defines the module name
 	ModuleName = "randomness"
@@ -12,6 +10,3 @@ const (
 	// RouterKey defines the module's message routing key
 	RouterKey = ModuleName
 )
-
-var SeedPrefix = collections.NewPrefix(0)
-var ValidatorVRFPrefix = collections.NewPrefix(1)
