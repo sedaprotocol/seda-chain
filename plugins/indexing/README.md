5751cb2150cca241979f653afa30505b8a2250cb
Thomas van Dam <thomas@seda.xyz>
2024-03-26 12:09:44 +0100

    chore(plugin): add simple logging implementation
    We wrap hclog as this seems to be the recommended library to use with
    the go-plugin architecture. We only support logging to file as any other
    destination doesn't end up where we would expect it to.
    
    Also includes some cleanup of the error handling as this commit touched
    that already anyway. Next commit will include fixes for the SQS error
    handling.
    
    Closes: #217

--- plugins/indexing/README.md
+++ plugins/indexing/README.md
@@ -13,9 +13,18 @@ The process that starts the node should have the following environment variables
 ```sh
 export COSMOS_SDK_ABCI=PATH_TO_PLUGIN_EXECUTABLE
 export SQS_QUEUE_URL=""
+export PLUGIN_LOG_FILE=PATH_TO_DESIRED_LOG_FILE
+# Optionally you can also specify the log level, one of "trace", "debug", "info", "warn", "error"
+export PLUGIN_LOG_LEVEL="WARN"
 ```
 
 Lastly, as we're using SQS the node needs access to a valid set of AWS credentials with permission to publish messages to the specified queue.
+
+### Logging
+
+The plugin uses [Hashicorp's hclog](https://pkg.go.dev/github.com/hashicorp/go-hclog) as this is the recommended approach in go-plugins and is also what the StreamingManager sets up on the Cosmos SDK side.
+
+Since we've been unable to get the plugin log output to show up in the node output for now we resort to logging to a file. A benefit is that this makes monitoring the plugin easier since we don't have to filter out any node logs. When deploying the plugin and configuring the log file make sure to set up log rotation for the log output directory.
 
 ## Building
 
--- plugins/indexing/plugin.go
+++ plugins/indexing/plugin.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/go-plugin"
 
 	streamingabci "cosmossdk.io/store/streaming/abci"
 	storetypes "cosmossdk.io/store/types"
-
 	"cosmossdk.io/x/tx/signing"
 	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -21,6 +21,7 @@ import (
 	"github.com/sedaprotocol/seda-chain/app/params"
 
 	bankmodule "github.com/sedaprotocol/seda-chain/plugins/indexing/bank"
+	log "github.com/sedaprotocol/seda-chain/plugins/indexing/log"
 	pluginsqs "github.com/sedaprotocol/seda-chain/plugins/indexing/sqs"
 	types "github.com/sedaprotocol/seda-chain/plugins/indexing/types"
 )
@@ -33,46 +34,65 @@ type IndexerPlugin struct {
 	blockHeight int64
 	cdc         codec.Codec
 	sqsClient   *pluginsqs.SqsClient
+	logger      *log.Logger
 }
 
 func (p *IndexerPlugin) ListenFinalizeBlock(_ context.Context, req abci.RequestFinalizeBlock, _ abci.ResponseFinalizeBlock) error {
+	p.logger.Debug(fmt.Sprintf("[%d] Start processing finalize block.", req.Height))
 	p.blockHeight = req.Height
 
+	p.logger.Debug(fmt.Sprintf("[%d] Processed finalize block.", req.Height))
 	return nil
 }
 
 func (p *IndexerPlugin) extractUpdate(change *storetypes.StoreKVPair) (*types.Message, error) {
 	switch change.StoreKey {
 	case bankmodule.StoreKey:
-		return bankmodule.ExtractUpdate(p.cdc, change)
+		return bankmodule.ExtractUpdate(p.cdc, p.logger, change)
 	default:
 		return nil, nil
 	}
 }
 
 func (p *IndexerPlugin) ListenCommit(_ context.Context, _ abci.ResponseCommit, changeSet []*storetypes.StoreKVPair) error {
+	p.logger.Debug(fmt.Sprintf("[%d] Start processing commit", p.blockHeight))
 	var messages []*types.Message
 
 	for _, change := range changeSet {
 		message, err := p.extractUpdate(change)
 		if err != nil {
+			p.logger.Error("Failed to extract update", "error", err)
 			return err
 		}
 
 		if message != nil {
+			p.logger.Debug("Extracted update", "message", message)
 			messages = append(messages, message)
 		}
 	}
 
 	publishError := p.sqsClient.PublishToQueue(p.blockHeight, messages)
 	if publishError != nil {
+		p.logger.Error("Failed to publish messages to queue.", "error", publishError)
 		return publishError
 	}
 
+	p.logger.Debug(fmt.Sprintf("[%d] Processed commit", p.blockHeight))
 	return nil
 }
 
 func main() {
+	logFile, err := log.GetLogFile()
+	if err != nil {
+		// Printing the error makes it easier to see what went wrong in the plugin output
+		fmt.Println(err)
+		panic(err)
+	}
+	defer logFile.Close()
+
+	logger := log.NewLogger(logFile)
+	logger.Info("initialising plugin")
+
 	// Configure address prefixes.
 	cfg := sdk.GetConfig()
 	cfg.SetBech32PrefixForAccount(params.Bech32PrefixAccAddr, params.Bech32PrefixAccPub)
@@ -93,7 +113,7 @@ func main() {
 		},
 	})
 	if err != nil {
-		panic(err)
+		logger.Fatal("failed to create interface registry", err)
 	}
 	std.RegisterInterfaces(interfaceRegistry)
 	app.ModuleBasics.RegisterInterfaces(interfaceRegistry)
@@ -101,8 +121,10 @@ func main() {
 	filePlugin := &IndexerPlugin{
 		cdc:       codec.NewProtoCodec(interfaceRegistry),
 		sqsClient: pluginsqs.NewSqsClient(),
+		logger:    logger,
 	}
 
+	logger.Info("finished initialising plugin")
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: streamingabci.Handshake,
 		Plugins: map[string]plugin.Plugin{
