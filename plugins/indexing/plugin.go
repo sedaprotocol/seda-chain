b10f69485663d49dd06d8db7183dc22c80a61f8f
Thomas van Dam <thomas@seda.xyz>
2024-03-26 21:03:03 +0100

    feat(plugin): publish block data
    Closes: #222

--- plugins/indexing/plugin.go
+++ plugins/indexing/plugin.go
@@ -21,6 +21,7 @@ import (
 	"github.com/sedaprotocol/seda-chain/app/params"
 
 	bankmodule "github.com/sedaprotocol/seda-chain/plugins/indexing/bank"
+	base "github.com/sedaprotocol/seda-chain/plugins/indexing/base"
 	log "github.com/sedaprotocol/seda-chain/plugins/indexing/log"
 	pluginsqs "github.com/sedaprotocol/seda-chain/plugins/indexing/sqs"
 	types "github.com/sedaprotocol/seda-chain/plugins/indexing/types"
@@ -37,9 +38,34 @@ type IndexerPlugin struct {
 	logger      *log.Logger
 }
 
+func (p *IndexerPlugin) publishToQueue(messages []*types.Message) error {
+	publishError := p.sqsClient.PublishToQueue(p.blockHeight, messages)
+	if publishError != nil {
+		p.logger.Error("Failed to publish messages to queue.", "error", publishError)
+		return publishError
+	}
+
+	return nil
+}
+
 func (p *IndexerPlugin) ListenFinalizeBlock(_ context.Context, req abci.RequestFinalizeBlock, _ abci.ResponseFinalizeBlock) error {
 	p.logger.Debug(fmt.Sprintf("[%d] Start processing finalize block.", req.Height))
 	p.blockHeight = req.Height
+	var messages []*types.Message
+
+	blockMessage, err := base.ExtractBlockUpdate(req)
+	if err != nil {
+		p.logger.Error("Failed to extract block update", "error", err)
+		return err
+	}
+	messages = append(messages, blockMessage)
+
+	// TODO(#223) Extract all transaction data.
+	// TODO(#229) Extract all vote data.
+
+	if err := p.publishToQueue(messages); err != nil {
+		return err
+	}
 
 	p.logger.Debug(fmt.Sprintf("[%d] Processed finalize block.", req.Height))
 	return nil
@@ -71,10 +97,8 @@ func (p *IndexerPlugin) ListenCommit(_ context.Context, _ abci.ResponseCommit, c
 		}
 	}
 
-	publishError := p.sqsClient.PublishToQueue(p.blockHeight, messages)
-	if publishError != nil {
-		p.logger.Error("Failed to publish messages to queue.", "error", publishError)
-		return publishError
+	if err := p.publishToQueue(messages); err != nil {
+		return err
 	}
 
 	p.logger.Debug(fmt.Sprintf("[%d] Processed commit", p.blockHeight))
