d18ee12a5ef4340609fec5a6350f0b6b4de21e36
Thomas van Dam <thomas@seda.xyz>
2024-03-26 21:03:03 +0100

    feat(plugin): publish block data
    Closes: #222

---
+++ plugins/indexing/base/block.go
@@ -0,0 +1,35 @@
+package base
+
+import (
+	"encoding/hex"
+	"strings"
+	"time"
+
+	abci "github.com/cometbft/cometbft/abci/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	types "github.com/sedaprotocol/seda-chain/plugins/indexing/types"
+)
+
+func ExtractBlockUpdate(req abci.RequestFinalizeBlock) (*types.Message, error) {
+	hash := strings.ToUpper(hex.EncodeToString(req.Hash))
+	txCount := len(req.Txs)
+	proposerAddress, err := sdk.ConsAddressFromHex(hex.EncodeToString(req.ProposerAddress))
+	if err != nil {
+		return nil, err
+	}
+
+	data := struct {
+		Hash            string    `json:"hash"`
+		Time            time.Time `json:"time"`
+		TxCount         int       `json:"txCount"`
+		ProposerAddress string    `json:"proposerAddress"`
+	}{
+		Hash:            hash,
+		Time:            req.Time,
+		TxCount:         txCount,
+		ProposerAddress: proposerAddress.String(),
+	}
+
+	return types.NewMessage("block", data), nil
+}
