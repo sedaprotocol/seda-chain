1effb1353ec9239c78229071bc1937553b345152
hacheigriega <hykim.home@gmail.com>
2024-05-17 15:15:56 -0400

    feat: sample data request wasm execution in endblock

--- x/wasm-storage/keeper/abci.go
+++ x/wasm-storage/keeper/abci.go
@@ -1,8 +1,15 @@
 package keeper
 
 import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"os"
+
 	"cosmossdk.io/collections"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	vm "github.com/sedaprotocol/seda-wasm-vm/bind_go"
 )
 
 func (k Keeper) EndBlock(ctx sdk.Context) error {
@@ -11,8 +18,10 @@ func (k Keeper) EndBlock(ctx sdk.Context) error {
 		return err
 	}
 
-	// TODO: Add Tally VM
-
+	err = k.ExecuteTally(ctx)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -30,5 +39,26 @@ func (k Keeper) ProcessExpiredWasms(ctx sdk.Context) error {
 			return err
 		}
 	}
+	return nil
+}
+
+func (k Keeper) ExecuteTally(ctx sdk.Context) error {
+	hash, err := hex.DecodeString("aad4d8a759c33a28bd6f6213c60e4e2f64d690ab559fc62d272a7d278170b802")
+	if err != nil {
+		return err
+	}
+	tallyWasm, err := k.DataRequestWasm.Get(ctx, hash)
+	if err != nil {
+		// TODO: reactivate error handling
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil
+		}
+		return err
+	}
+
+	result := vm.ExecuteTallyVm(tallyWasm.Bytecode, []string{"1", "2"}, map[string]string{
+		"PATH": os.Getenv("SHELL"),
+	})
+	fmt.Println(result)
 	return nil
 }
