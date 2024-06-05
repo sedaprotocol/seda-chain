64225c0e63b89048063cdb7425cb8ebdaa1dec80
hacheigriega <hykim.home@gmail.com>
2024-06-05 16:52:18 -0400

    refactor: rename mock files

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -8,5 +8,5 @@ if ! [ -x "$(command -v $mockgen_cmd)" ]; then
 fi
 
 # Generate mocks for the given package
-$mockgen_cmd -source=$GOPATH/pkg/mod/github.com/\!cosm\!wasm/wasmd@v0.50.0/x/wasm/types/exported_keepers.go -package testutil -destination x/wasm-storage/testutil/expected_keepers_mock.go
-$mockgen_cmd -source=x/wasm-storage/types/expected_keepers.go -package testutil -destination x/wasm-storage/testutil/expected_keepers_mock_2.go
+$mockgen_cmd -source=$GOPATH/pkg/mod/github.com/\!cosm\!wasm/wasmd@v0.50.0/x/wasm/types/exported_keepers.go -package testutil -destination x/wasm-storage/keeper/testutil/wasm_keepers_mock.go
+$mockgen_cmd -source=x/wasm-storage/types/expected_keepers.go -package testutil -destination x/wasm-storage/keeper/testutil/expected_keepers_mock.go
