f77ea0fca03d0b9eb1834e958095940f27472d7b
hacheigriega <hykim.home@gmail.com>
2024-07-02 22:30:31 -0400

    fix: return panic msg correctly after recover

--- x/wasm-storage/keeper/abci.go
+++ x/wasm-storage/keeper/abci.go
@@ -14,17 +14,18 @@ import (
 )
 
 func (k Keeper) EndBlock(ctx sdk.Context) (err error) {
+	// Use defer to prevent returning an error, which would cause
+	// the chain to halt.
 	defer func() {
 		// Handle a panic.
 		if r := recover(); r != nil {
-			k.Logger(ctx).Error("recovered from panic in wasm-storage EndBlock", "err", err)
-			err = nil
+			k.Logger(ctx).Error("recovered from panic in wasm-storage EndBlock", "err", r)
 		}
 		// Handle an error.
 		if err != nil {
 			k.Logger(ctx).Error("error in wasm-storage EndBlock", "err", err)
-			err = nil
 		}
+		err = nil
 	}()
 
 	err = k.ProcessExpiredWasms(ctx)
@@ -35,7 +36,7 @@ func (k Keeper) EndBlock(ctx sdk.Context) (err error) {
 	if err != nil {
 		return
 	}
-	return nil
+	return
 }
 
 func (k Keeper) ProcessExpiredWasms(ctx sdk.Context) error {
