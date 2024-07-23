dc9792abd7530ef42b0ef7b502b4cde6210bd007
hacheigriega <hykim.home@gmail.com>
2024-07-23 13:42:25 -0400

    WIP: swagger doc

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -1,30 +1,72 @@
 #!/usr/bin/env bash
+SWAGGER_DIR=./swagger-proto
 
-set -eo pipefail
+set -eox pipefail
 
+# prepare swagger generation
+mkdir -p "$SWAGGER_DIR/proto"
+printf "version: v1\ndirectories:\n  - proto\n  - third_party" > "$SWAGGER_DIR/buf.work.yaml"
+printf "version: v1\nname: buf.build/sedachain/sedachain\n" > "$SWAGGER_DIR/proto/buf.yaml"
+cp ./proto/buf.gen.swagger.yaml "$SWAGGER_DIR/proto/buf.gen.swagger.yaml"
+
+# copy existing proto files
+cp -r ./proto/sedachain "$SWAGGER_DIR/proto"
+
+# create temporary folder to store intermediate results from `buf generate`
 mkdir -p ./tmp-swagger-gen
-cd proto
-seda_proto_dirs=$(find ./sedachain -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 
-SDK_VERSION=$(go list -m github.com/cosmos/cosmos-sdk | sed 's/.* //')
-COSMOS_PROTO_DIR=$(go env GOPATH)/pkg/mod/github.com/cosmos/cosmos-sdk@$SDK_VERSION/proto
-cosmos_proto_dirs=$(find $COSMOS_PROTO_DIR/cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+# step into swagger folder
+cd "$SWAGGER_DIR"
 
-proto_dirs=("${seda_proto_dirs[@]}" "${cosmos_proto_dirs[@]}")
-echo $proto_dirs
+# create swagger files on an individual basis  w/ `buf build` and `buf generate` (needed for `swagger-combine`)
+proto_dirs=$(find ./proto ./third_party -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
-  if [[ ! -z "$query_file" ]]; then
-    buf generate --template buf.gen.swagger.yaml $query_file
+  if [[ -n "$query_file" ]]; then
+    buf generate --template proto/buf.gen.swagger.yaml "$query_file"
   fi
 done
 
 cd ..
+
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
-swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+swagger-combine ./client/docs/config.json -o ./client/docs/static/swagger/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
 
 # clean swagger files
-# rm -rf ./tmp-swagger-gen
+rm -rf ./tmp-swagger-gen
+rm -rf "$SWAGGER_DIR"
+
+
+# #!/usr/bin/env bash
+
+# set -eo pipefail
+
+# mkdir -p ./tmp-swagger-gen
+# cd proto
+# seda_proto_dirs=$(find ./sedachain -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+
+# SDK_VERSION=$(go list -m github.com/cosmos/cosmos-sdk | sed 's/.* //')
+# COSMOS_PROTO_DIR=$(go env GOPATH)/pkg/mod/github.com/cosmos/cosmos-sdk@$SDK_VERSION/proto
+# cosmos_proto_dirs=$(find $COSMOS_PROTO_DIR/cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+
+# proto_dirs=("${seda_proto_dirs[@]}" "${cosmos_proto_dirs[@]}")
+# echo $proto_dirs
+# for dir in $proto_dirs; do
+#   # generate swagger files (filter query files)
+#   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+#   if [[ ! -z "$query_file" ]]; then
+#     buf generate --template buf.gen.swagger.yaml $query_file
+#   fi
+# done
+
+# cd ..
+# # combine swagger files
+# # uses nodejs package `swagger-combine`.
+# # all the individual swagger files need to be configured in `config.json` for merging
+# swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+
+# # clean swagger files
+# # rm -rf ./tmp-swagger-gen
