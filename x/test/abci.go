4de04793b20937887e6ed9add6bf4539340206fd
hacheigriega <hykim.home@gmail.com>
2024-08-14 17:34:45 -0400

    chore: add comments for clarification

--- x/test/abci.go
+++ x/test/abci.go
@@ -22,12 +22,18 @@ func NewDefaultProposalHandler(txVerifier baseapp.ProposalTxVerifier) *ProposalH
 
 func (h *ProposalHandler) PrepareProposalHandler(txConfig client.TxConfig) sdk.PrepareProposalHandler {
 	return func(ctx sdk.Context, req *abci.RequestPrepareProposal) (*abci.ResponsePrepareProposal, error) {
+		//
+		// fill in here
+		//
 		return &abci.ResponsePrepareProposal{Txs: [][]byte{}}, nil
 	}
 }
 
 func (h *ProposalHandler) ProcessProposalHandler(txConfig client.TxConfig) sdk.ProcessProposalHandler {
 	return func(ctx sdk.Context, req *abci.RequestProcessProposal) (*abci.ResponseProcessProposal, error) {
+		//
+		// fill in here
+		//
 		return &abci.ResponseProcessProposal{Status: abci.ResponseProcessProposal_ACCEPT}, nil
 	}
 }
