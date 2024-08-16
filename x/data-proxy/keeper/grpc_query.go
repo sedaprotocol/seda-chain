6d3372cccca9452a72a570e4394c914d554f428c
Thomas van Dam <thomas@seda.xyz>
2024-08-16 17:53:17 +0200

    chore(data-proxy): update proxy query not found error
    Also conform to new import formatting rules.
    
    Part-of: #316

--- x/data-proxy/keeper/grpc_query.go
+++ x/data-proxy/keeper/grpc_query.go
@@ -2,6 +2,9 @@ package keeper
 
 import (
 	"context"
+	"errors"
+
+	"cosmossdk.io/collections"
 
 	"github.com/sedaprotocol/seda-chain/x/data-proxy/types"
 )
@@ -15,7 +18,12 @@ type Querier struct {
 func (q Querier) DataProxyConfig(ctx context.Context, req *types.QueryDataProxyConfigRequest) (*types.QueryDataProxyConfigResponse, error) {
 	result, err := q.GetDataProxyConfig(ctx, req.PubKey)
 	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil, types.ErrUnknownDataProxy.Wrapf("no data proxy registered for %s", req.PubKey)
+		}
+
 		return nil, err
 	}
+
 	return &types.QueryDataProxyConfigResponse{Config: &result}, nil
 }
