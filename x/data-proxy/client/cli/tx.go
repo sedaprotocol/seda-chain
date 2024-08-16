23cfbbb671f65249421df48ef8474fe890f61a7d
Thomas van Dam <thomas@seda.xyz>
2024-08-16 16:47:03 +0200

    chore(data-proxy): add registration message handling
    Also introduces an admin address to make updates to the proxy config
    easier for operators. An alternative would be to use a sequence number,
    but that makes the whole managamenet process more cumbersome for the
    operator.
    
    Part-of: #316

--- x/data-proxy/client/cli/tx.go
+++ x/data-proxy/client/cli/tx.go
@@ -8,8 +8,14 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/sedaprotocol/seda-chain/x/data-proxy/types"
+)
+
+const (
+	// FlagKeyFile defines a flag to add arbitrary data to a data proxy.
+	FlagMemo = "memo"
 )
 
 // GetTxCmd returns the CLI transaction commands for this module
@@ -29,24 +35,38 @@ func GetTxCmd() *cobra.Command {
 // AddKey returns the command for adding a new key and uploading its
 // public key on chain at a given index.
 func RegisterDataProxy() *cobra.Command {
-	// TODO
 	cmd := &cobra.Command{
-		Use:   "register [thing]",
+		Use:   "register [payout_address] [fee] [public_key_hex] [signature_hex] --from [admin_address]",
 		Short: "Register a data proxy using a signed message generated with the data-proxy cli",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			// TODO
-			msg := &types.MsgRegisterDataProxy{}
+			fee, err := sdk.ParseCoinNormalized(args[1])
+			if err != nil {
+				return err
+			}
+
+			memo, _ := cmd.Flags().GetString(FlagMemo)
+
+			msg := &types.MsgRegisterDataProxy{
+				AdminAddress:  clientCtx.GetFromAddress().String(),
+				PayoutAddress: args[0],
+				Fee:           &fee,
+				PubKey:        args[2],
+				Signature:     args[3],
+				Memo:          memo,
+			}
+
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
 
 	flags.AddTxFlagsToCmd(cmd)
+	cmd.Flags().String(FlagMemo, "", "Optionally add a description to the data proxy config")
 	return cmd
 }
 
