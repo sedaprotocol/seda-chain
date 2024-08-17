fd06bf50f90853ca65afe211377b037df7e5bcd0
Thomas van Dam <thomas@seda.xyz>
2024-08-17 21:58:44 +0200

    chore(data-proxy): add genesis import and export
    Part-of: #316

--- x/data-proxy/keeper/genesis.go
+++ x/data-proxy/keeper/genesis.go
@@ -1,21 +1,101 @@
 package keeper
 
 import (
+	"cosmossdk.io/collections"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/sedaprotocol/seda-chain/x/data-proxy/types"
 )
 
 // InitGenesis initializes the store based on the given genesis state.
-func (k Keeper) InitGenesis(_ sdk.Context, _ types.GenesisState) {
-	// TODO
+func (k Keeper) InitGenesis(ctx sdk.Context, data types.GenesisState) {
+	if err := k.Params.Set(ctx, data.Params); err != nil {
+		panic(err)
+	}
+
+	for _, dataProxyConfig := range data.DataProxyConfigs {
+		if err := k.DataProxyConfigs.Set(ctx, dataProxyConfig.DataProxyPubkey, *dataProxyConfig.Config); err != nil {
+			panic(err)
+		}
+	}
+
+	for _, feeUpdate := range data.FeeUpdateQueue {
+		if err := k.FeeUpdateQueue.Set(ctx, collections.Join(feeUpdate.UpdateHeight, feeUpdate.DataProxyPubkey)); err != nil {
+			panic(err)
+		}
+	}
 }
 
 // ExportGenesis extracts all data from store to genesis state.
-func (k Keeper) ExportGenesis(_ sdk.Context) types.GenesisState {
+func (k Keeper) ExportGenesis(ctx sdk.Context) types.GenesisState {
 	var gs types.GenesisState
 
-	// TODO
+	params, err := k.Params.Get(ctx)
+	if err != nil {
+		panic(err)
+	}
+	gs.Params = params
+
+	configs, err := k.getAllDataProxyConfigs(ctx)
+	if err != nil {
+		panic(err)
+	}
+	gs.DataProxyConfigs = configs
+
+	feeUpdates, err := k.getAllFeeUpdateRecords(ctx)
+	if err != nil {
+		panic(err)
+	}
+	gs.FeeUpdateQueue = feeUpdates
 
 	return gs
+}
+
+func (k Keeper) getAllDataProxyConfigs(ctx sdk.Context) ([]types.DataProxyConfig, error) {
+	configs := make([]types.DataProxyConfig, 0)
+
+	itr, err := k.DataProxyConfigs.Iterate(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer itr.Close()
+
+	for ; itr.Valid(); itr.Next() {
+		kv, err := itr.KeyValue()
+		if err != nil {
+			return nil, err
+		}
+
+		configs = append(configs, types.DataProxyConfig{
+			DataProxyPubkey: kv.Key,
+			Config:          &kv.Value,
+		})
+	}
+
+	return configs, nil
+}
+
+func (k Keeper) getAllFeeUpdateRecords(ctx sdk.Context) ([]types.FeeUpdateQueueRecord, error) {
+	feeUpdates := make([]types.FeeUpdateQueueRecord, 0)
+
+	itr, err := k.FeeUpdateQueue.Iterate(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer itr.Close()
+
+	for ; itr.Valid(); itr.Next() {
+		key, err := itr.Key()
+		if err != nil {
+			return nil, err
+		}
+
+		feeUpdates = append(feeUpdates, types.FeeUpdateQueueRecord{
+			UpdateHeight:    key.K1(),
+			DataProxyPubkey: key.K2(),
+		})
+	}
+
+	return feeUpdates, nil
 }
