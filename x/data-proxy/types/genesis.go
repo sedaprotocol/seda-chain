d2b0bced89c68cf90af058edde37489cace006d5
Thomas van Dam <thomas@seda.xyz>
2024-08-17 21:58:44 +0200

    chore(data-proxy): add genesis import and export
    Part-of: #316

--- x/data-proxy/types/genesis.go
+++ x/data-proxy/types/genesis.go
@@ -1,14 +1,47 @@
 package types
 
-func NewGenesisState() GenesisState {
-	return GenesisState{}
+import "fmt"
+
+func NewGenesisState(params Params, proxyConfigs []DataProxyConfig, feeUpdates []FeeUpdateQueueRecord) GenesisState {
+	return GenesisState{
+		Params:           params,
+		DataProxyConfigs: proxyConfigs,
+		FeeUpdateQueue:   feeUpdates,
+	}
 }
 
 func DefaultGenesisState() *GenesisState {
-	state := NewGenesisState()
+	state := NewGenesisState(DefaultParams(), []DataProxyConfig{}, []FeeUpdateQueueRecord{})
 	return &state
 }
 
-func ValidateGenesis(_ GenesisState) error {
-	return nil
+func ValidateGenesis(data GenesisState) error {
+	for _, proxyConfig := range data.DataProxyConfigs {
+		if len(proxyConfig.DataProxyPubkey) == 0 {
+			return fmt.Errorf("empty public key in proxy configs")
+		}
+
+		if err := proxyConfig.Config.Validate(); err != nil {
+			return err
+		}
+
+		if err := proxyConfig.Config.Fee.Validate(); err != nil {
+			return err
+		}
+
+		if proxyConfig.Config.AdminAddress == "" {
+			return fmt.Errorf("empty admin address")
+		}
+		if proxyConfig.Config.PayoutAddress == "" {
+			return fmt.Errorf("empty payout address")
+		}
+	}
+
+	for _, feeUpdate := range data.FeeUpdateQueue {
+		if len(feeUpdate.DataProxyPubkey) == 0 {
+			return fmt.Errorf("empty public key in fee updates")
+		}
+	}
+
+	return data.Params.Validate()
 }
