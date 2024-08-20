c8b5a9a979900723766c46a825b0d7ad8b882730
Thomas van Dam <thomas@seda.xyz>
2024-08-20 15:01:24 +0200

    chore(data-proxy): emit events on TXs and endblock changes
    Part-of: #316

--- x/data-proxy/types/events.go
+++ x/data-proxy/types/events.go
@@ -1,5 +1,16 @@
 package types
 
 const (
-	EventTypeRegisterProxy = "register_proxy"
+	EventTypeRegisterProxy = "register_data_proxy"
+	EventTypeEditProxy     = "edit_data_proxy"
+	EventTypeFeeUpdate     = "fee_update"
+	EventTypeTransferAdmin = "transfer_admin"
+
+	AttributePubKey        = "pub_key"
+	AttributePayoutAddress = "payout_address"
+	AttributeFee           = "fee"
+	AttributeMemo          = "memo"
+	AttributeAdminAddress  = "admin_address"
+	AttributeNewFee        = "new_fee"
+	AttributeNewFeeHeight  = "new_fee_height"
 )
