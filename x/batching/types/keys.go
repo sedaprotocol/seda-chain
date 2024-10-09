00b276b41485ee0aed57f8523697298993bd97ca
hacheigriega <hykim.home@gmail.com>
2024-10-09 09:38:07 -0400

    feat: persisting batch signatures

--- x/batching/types/keys.go
+++ x/batching/types/keys.go
@@ -11,11 +11,11 @@ const (
 )
 
 var (
-	DataResultsPrefix     = collections.NewPrefix(0)
-	CurrentBatchNumberKey = collections.NewPrefix(1)
-	BatchesKeyPrefix      = collections.NewPrefix(2)
-	BatchNumberKeyPrefix  = collections.NewPrefix(3)
-	TreeEntriesKeyPrefix  = collections.NewPrefix(4)
-	VotesKeyPrefix        = collections.NewPrefix(5)
-	ParamsKey             = collections.NewPrefix(6)
+	DataResultsPrefix        = collections.NewPrefix(0)
+	CurrentBatchNumberKey    = collections.NewPrefix(1)
+	BatchesKeyPrefix         = collections.NewPrefix(2)
+	BatchNumberKeyPrefix     = collections.NewPrefix(3)
+	TreeEntriesKeyPrefix     = collections.NewPrefix(4)
+	BatchSignaturesKeyPrefix = collections.NewPrefix(5)
+	ParamsKey                = collections.NewPrefix(6)
 )
