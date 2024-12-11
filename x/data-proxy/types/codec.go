6b885fa34b246832cacae492bbe859be0b7fd98f
Thomas van Dam <thomas@seda.xyz>
2024-12-11 21:08:10 +0100

    feat(data-proxy): support amino signing for transactions

--- x/data-proxy/types/codec.go
+++ x/data-proxy/types/codec.go
@@ -2,12 +2,16 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
-func RegisterCodec(_ *codec.LegacyAmino) {
+func RegisterLegacyCodec(cdc *codec.LegacyAmino) {
+	legacy.RegisterAminoMsg(cdc, &MsgRegisterDataProxy{}, "sedachain/MsgRegisterDataProxy")
+	legacy.RegisterAminoMsg(cdc, &MsgEditDataProxy{}, "sedachain/MsgEditDataProxy")
+	legacy.RegisterAminoMsg(cdc, &MsgTransferAdmin{}, "sedachain/MsgTransferAdmin")
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
