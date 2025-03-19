c66f1b0680406478bcbdf1a8649eea875228b5f5
murongshaozong <netease163@icloud.com>
2025-03-19 17:17:46 +0800

    chore: make function comment match function name
    Signed-off-by: murongshaozong <netease163@icloud.com>

--- x/vesting/types/codec.go
+++ x/vesting/types/codec.go
@@ -21,7 +21,7 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	legacy.RegisterAminoMsg(cdc, &MsgCreateVestingAccount{}, "cosmos-sdk/MsgCreateVestingAccount")
 }
 
-// RegisterInterface associates protoName with AccountI and VestingAccount
+// RegisterInterfaces associates protoName with AccountI and VestingAccount
 // Interfaces and creates a registry of it's concrete implementations
 func RegisterInterfaces(registry types.InterfaceRegistry) {
 	registry.RegisterInterface(
