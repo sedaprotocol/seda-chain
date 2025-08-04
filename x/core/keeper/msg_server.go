97af6b5cd2dc16d399ac7c19d2490a67f7ad236a
hacheigriega <hykim.home@gmail.com>
2025-08-04 12:34:31 -0400

    feat(x/core): store Stakers and message MsgStake

--- x/core/keeper/msg_server.go
+++ x/core/keeper/msg_server.go
@@ -2,21 +2,25 @@ package keeper
 
 import (
 	"context"
+	"encoding/hex"
+	"errors"
+	"strconv"
 
+	vrf "github.com/sedaprotocol/vrf-go"
+
+	"cosmossdk.io/collections"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/sedaprotocol/seda-chain/x/core/types"
 )
 
+var _ types.MsgServer = msgServer{}
+
 type msgServer struct {
 	Keeper
 }
 
-var _ types.MsgServer = msgServer{}
-
-// NewMsgServerImpl returns an implementation of the MsgServer interface
-// for the provided Keeper.
 func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{Keeper: keeper}
 }
@@ -26,6 +30,10 @@ func (m msgServer) AddToAllowlist(goCtx context.Context, msg *types.MsgAddToAllo
 
 	if msg.Sender != m.GetAuthority() {
 		return nil, sdkerrors.ErrUnauthorized.Wrapf("unauthorized authority; expected %s, got %s", m.GetAuthority(), msg.Sender)
+	}
+
+	if msg.PublicKey == "" {
+		return nil, sdkerrors.ErrInvalidRequest.Wrapf("public key is empty")
 	}
 
 	exists, err := m.Allowlist.Has(ctx, msg.PublicKey)
@@ -41,15 +49,118 @@ func (m msgServer) AddToAllowlist(goCtx context.Context, msg *types.MsgAddToAllo
 		return nil, err
 	}
 
-	// TODO: add event
-	// Ok(Response::new().add_attribute("action", "add-to-allowlist").add_event(
-	// 	Event::new("seda-contract").add_attributes([
-	// 		("version", CONTRACT_VERSION.to_string()),
-	// 		("identity", self.public_key),
-	// 		("action", "allowlist-add".to_string()),
-	// 	]),
-	// ))
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeAddToAllowlist,
+			sdk.NewAttribute(types.AttributeIdentity, msg.PublicKey),
+		),
+	)
+
 	return &types.MsgAddToAllowlistResponse{}, nil
+}
+
+func (m msgServer) Stake(goCtx context.Context, msg *types.MsgStake) (*types.MsgStakeResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Verify stake proof.
+	var sequenceNum uint64
+	var isExistingStaker bool
+	staker, err := m.Stakers.Get(ctx, msg.PublicKey)
+	if err != nil {
+		if !errors.Is(err, collections.ErrNotFound) {
+			return nil, err
+		}
+	} else {
+		sequenceNum = staker.SequenceNum
+		isExistingStaker = true
+	}
+
+	hash, err := msg.ComputeStakeHash(ctx.ChainID(), sequenceNum)
+	if err != nil {
+		return nil, err
+	}
+	publicKey, err := hex.DecodeString(msg.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+	proof, err := hex.DecodeString(msg.Proof)
+	if err != nil {
+		return nil, err
+	}
+	_, err = vrf.NewK256VRF().Verify(publicKey, proof, hash)
+	if err != nil {
+		return nil, types.ErrInvalidStakeProof.Wrapf(err.Error())
+	}
+
+	// Verify that the staker is allowlisted if allowlist is enabled.
+	params, err := m.Params.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if params.StakingConfig.AllowlistEnabled {
+		allowlisted, err := m.Allowlist.Has(ctx, msg.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		if !allowlisted {
+			return nil, types.ErrNotAllowlisted
+		}
+	}
+
+	minStake := params.StakingConfig.MinimumStake
+	denom, err := m.stakingKeeper.BondDenom(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if msg.Stake.Denom != denom {
+		return nil, sdkerrors.ErrInvalidCoins.Wrapf("invalid denom: %s", msg.Stake.Denom)
+	}
+
+	// Check stake amount and save the staker.
+	if isExistingStaker {
+		staker.Staked = staker.Staked.Add(msg.Stake)
+		staker.Memo = msg.Memo
+	} else {
+		if msg.Stake.Amount.LT(minStake) {
+			return nil, types.ErrInsufficientStake.Wrapf("%s is less than minimum stake %s", msg.Stake.Amount, minStake)
+		}
+		staker = types.Staker{
+			PublicKey:         msg.PublicKey,
+			Memo:              msg.Memo,
+			Staked:            msg.Stake,
+			PendingWithdrawal: sdk.NewInt64Coin(denom, 0),
+			SequenceNum:       sequenceNum,
+		}
+	}
+
+	senderAddr, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return nil, sdkerrors.ErrInvalidAddress.Wrapf("invalid sender address: %s", msg.Sender)
+	}
+	err = m.bankKeeper.SendCoinsFromAccountToModule(ctx, senderAddr, types.ModuleName, sdk.NewCoins(msg.Stake))
+	if err != nil {
+		return nil, err
+	}
+
+	staker.SequenceNum = sequenceNum + 1
+	err = m.Stakers.Set(ctx, msg.PublicKey, staker)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeStake,
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Sender),
+			sdk.NewAttribute(sdk.AttributeKeyAmount, msg.Stake.String()),
+			sdk.NewAttribute(types.AttributeIdentity, msg.PublicKey),
+			sdk.NewAttribute(types.AttributeTokensStaked, staker.Staked.String()),
+			sdk.NewAttribute(types.AttributeTokensPendingWithdrawal, staker.PendingWithdrawal.String()),
+			sdk.NewAttribute(types.AttributeMemo, staker.Memo),
+			sdk.NewAttribute(types.AttributeSequenceNumber, strconv.FormatUint(sequenceNum, 10)),
+		),
+	)
+	return &types.MsgStakeResponse{}, nil
 }
 
 func (m msgServer) UpdateParams(goCtx context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
--- x/core/types/errors.go
+++ x/core/types/errors.go
@@ -4,4 +4,9 @@ import (
 	"cosmossdk.io/errors"
 )
 
-var ErrAlreadyAllowlisted = errors.Register("core", 2, "public key already exists in allowlist")
+var (
+	ErrAlreadyAllowlisted = errors.Register("core", 2, "public key already exists in allowlist")
+	ErrNotAllowlisted     = errors.Register("core", 3, "public key is not in allowlist")
+	ErrInsufficientStake  = errors.Register("core", 4, "stake amount is insufficient")
+	ErrInvalidStakeProof  = errors.Register("core", 5, "invalid stake proof")
+)
