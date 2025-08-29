2afba1ebaa2a81d2eeba31c4ab5eea0e94b4e7ca
Thomas van Dam <thomas@seda.xyz>
2025-08-29 13:08:09 +0200

    feat(x/sophon): add user queries
    Part-of: #595

--- x/sophon/keeper/grpc_query.go
+++ x/sophon/keeper/grpc_query.go
@@ -10,6 +10,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
 
 	"github.com/sedaprotocol/seda-chain/x/sophon/types"
 )
@@ -36,7 +37,7 @@ func (q Querier) SophonInfo(c context.Context, req *types.QuerySophonInfoRequest
 		return nil, err
 	}
 
-	return &types.QuerySophonInfoResponse{Info: &result}, nil
+	return &types.QuerySophonInfoResponse{Info: result}, nil
 }
 
 func (q Querier) SophonTransfer(c context.Context, req *types.QuerySophonTransferRequest) (*types.QuerySophonTransferResponse, error) {
@@ -69,12 +70,70 @@ func (q Querier) SophonTransfer(c context.Context, req *types.QuerySophonTransfe
 	return &types.QuerySophonTransferResponse{NewOwnerAddress: newOwnerAddress}, nil
 }
 
-func (q Querier) SophonUsers(_ context.Context, _ *types.QuerySophonUsersRequest) (*types.QuerySophonUsersResponse, error) {
-	panic("not implemented")
+func (q Querier) SophonUsers(c context.Context, req *types.QuerySophonUsersRequest) (*types.QuerySophonUsersResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+
+	pubKeyBytes, err := hex.DecodeString(req.SophonPubKey)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "invalid hex in pubkey: %s", req.SophonPubKey)
+	}
+
+	sophonInfo, err := q.GetSophonInfo(ctx, pubKeyBytes)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil, sdkerrors.ErrNotFound.Wrapf("no sophon registered for %s", req.SophonPubKey)
+		}
+
+		return nil, err
+	}
+
+	users, pageRes, err := query.CollectionPaginate(
+		ctx, q.sophonUser, req.Pagination,
+		func(_ collections.Pair[uint64, string], value types.SophonUser) (types.SophonUser, error) {
+			return value, nil
+		},
+		func(opts *query.CollectionsPaginateOptions[collections.Pair[uint64, string]]) {
+			prefix := collections.PairPrefix[uint64, string](sophonInfo.Id)
+			opts.Prefix = &prefix
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QuerySophonUsersResponse{
+		Users:      users,
+		Pagination: pageRes,
+	}, nil
 }
 
-func (q Querier) SophonUser(_ context.Context, _ *types.QuerySophonUserRequest) (*types.QuerySophonUserResponse, error) {
-	panic("not implemented")
+func (q Querier) SophonUser(c context.Context, req *types.QuerySophonUserRequest) (*types.QuerySophonUserResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+
+	pubKeyBytes, err := hex.DecodeString(req.SophonPubKey)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "invalid hex in pubkey: %s", req.SophonPubKey)
+	}
+
+	sophonInfo, err := q.GetSophonInfo(ctx, pubKeyBytes)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil, sdkerrors.ErrNotFound.Wrapf("no sophon registered for %s", req.SophonPubKey)
+		}
+
+		return nil, err
+	}
+
+	user, err := q.GetSophonUser(ctx, sophonInfo.Id, req.UserId)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil, sdkerrors.ErrNotFound.Wrapf("no user registered for %s", req.UserId)
+		}
+
+		return nil, err
+	}
+
+	return &types.QuerySophonUserResponse{User: user}, nil
 }
 
 func (q Querier) SophonEligibility(_ context.Context, _ *types.QuerySophonEligibilityRequest) (*types.QuerySophonEligibilityResponse, error) {
