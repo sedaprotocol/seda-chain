803eb28e12400220bf245649d1f496fac081ffb2
hacheigriega <hykim.home@gmail.com>
2025-09-08 12:42:08 -0400

    feat(x/wasm): custom x/wasm keeper and contract msg redirect
    We use the wasm module msg server wrapper to intercept, encode, and
    dispatch Core Contract messages to corresponding handlers in x/core.
    We also start using the wasm keeper wrapper to support certain
    operations here, such as identification of Core Contract address.

--- x/wasm/keeper.go
+++ x/wasm/keeper.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/CosmWasm/wasmd/x/wasm/keeper"
 
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -13,14 +14,31 @@ type WasmStorageKeeper interface {
 	GetCoreContractAddr(ctx context.Context) (sdk.AccAddress, error)
 }
 
-type Keeper struct {
-	*keeper.Keeper
-	WasmStorageKeeper WasmStorageKeeper
+type StakingKeeper interface {
+	BondDenom(ctx context.Context) (string, error)
 }
 
-func NewKeeper(k *keeper.Keeper, wsk WasmStorageKeeper) *Keeper {
+type Keeper struct {
+	*keeper.Keeper                         // default wasm keeper
+	WasmStorageKeeper WasmStorageKeeper    // for core contract shim
+	StakingKeeper     StakingKeeper        // for core contract shim
+	cdc               codec.Codec          // for core contract shim
+	router            keeper.MessageRouter // for core contract shim
+}
+
+func NewKeeper(k *keeper.Keeper, sk StakingKeeper, cdc codec.Codec, router keeper.MessageRouter) *Keeper {
 	return &Keeper{
-		Keeper:            k,
-		WasmStorageKeeper: wsk,
+		Keeper:        k,
+		StakingKeeper: sk,
+		cdc:           cdc,
+		router:        router,
 	}
+}
+
+func (k *Keeper) SetWasmStorageKeeper(wsk WasmStorageKeeper) {
+	k.WasmStorageKeeper = wsk
+}
+
+func (k *Keeper) SetRouter(router keeper.MessageRouter) {
+	k.router = router
 }
---
+++ x/wasm/msgs.go
@@ -0,0 +1,174 @@
+package wasm
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+
+	"cosmossdk.io/math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	coretypes "github.com/sedaprotocol/seda-chain/x/core/types"
+)
+
+func base64ToHex(base64Str string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(base64Str)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(decoded), nil
+}
+
+type PostRequestResponsePayload struct {
+	DrID   string `json:"dr_id"`
+	Height uint64 `json:"height"`
+}
+
+type CoreContractMsg struct {
+	AddToAllowList   *AddToAllowListMsg   `json:"add_to_allowlist"`
+	Stake            *StakeMsg            `json:"stake"`
+	PostDataRequest  *PostDataRequestMsg  `json:"post_data_request"`
+	CommitDataResult *CommitDataResultMsg `json:"commit_data_result"`
+	RevealDataResult *RevealDataResultMsg `json:"reveal_data_result"`
+}
+
+type AddToAllowListMsg struct {
+	PublicKey string `json:"public_key"`
+}
+
+func (m AddToAllowListMsg) EncodeToSdkMsg(sender string) (sdk.Msg, error) {
+	return &coretypes.MsgAddToAllowlist{
+		Sender:    sender, // TODO ensure security
+		PublicKey: m.PublicKey,
+	}, nil
+}
+
+type StakeMsg struct {
+	PublicKey string `json:"public_key"`
+	Proof     string `json:"proof"`
+	Memo      string `json:"memo"`
+}
+
+func (m StakeMsg) EncodeToSdkMsg(sender string, stake sdk.Coin) (sdk.Msg, error) {
+	return &coretypes.MsgStake{
+		Sender:    sender, // TODO ensure security
+		PublicKey: m.PublicKey,
+		Proof:     m.Proof,
+		Memo:      m.Memo,
+		Stake:     stake,
+	}, nil
+}
+
+type PostDataRequestMsg struct {
+	PostedDR       PostedDR `json:"posted_dr"`
+	SedaPayload    string   `json:"seda_payload"`
+	PaybackAddress string   `json:"payback_address"`
+}
+
+type PostedDR struct {
+	Version           string `json:"version"`
+	ExecProgramID     string `json:"exec_program_id"`
+	ExecInputs        string `json:"exec_inputs"`
+	ExecGasLimit      uint64 `json:"exec_gas_limit"`
+	TallyProgramID    string `json:"tally_program_id"`
+	TallyInputs       string `json:"tally_inputs"`
+	TallyGasLimit     uint64 `json:"tally_gas_limit"`
+	ReplicationFactor uint16 `json:"replication_factor"`
+	ConsensusFilter   string `json:"consensus_filter"`
+	GasPrice          string `json:"gas_price"`
+	Memo              string `json:"memo"`
+}
+
+func (m PostDataRequestMsg) EncodeToSdkMsg(sender string, funds sdk.Coin) (sdk.Msg, error) {
+	execInputs, err := base64.StdEncoding.DecodeString(m.PostedDR.ExecInputs)
+	if err != nil {
+		return nil, err
+	}
+	tallyInputs, err := base64.StdEncoding.DecodeString(m.PostedDR.TallyInputs)
+	if err != nil {
+		return nil, err
+	}
+	consensusFilter, err := base64.StdEncoding.DecodeString(m.PostedDR.ConsensusFilter)
+	if err != nil {
+		return nil, err
+	}
+	memo, err := base64.StdEncoding.DecodeString(m.PostedDR.Memo)
+	if err != nil {
+		return nil, err
+	}
+	sedaPayload, err := base64.StdEncoding.DecodeString(m.SedaPayload)
+	if err != nil {
+		return nil, err
+	}
+	paybackAddress, err := base64.StdEncoding.DecodeString(m.PaybackAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	gasPriceInt, ok := math.NewIntFromString(m.PostedDR.GasPrice)
+	if !ok {
+		return nil, fmt.Errorf("failed to convert gas price to big.Int")
+	}
+
+	return &coretypes.MsgPostDataRequest{
+		Sender:            sender,
+		Funds:             funds,
+		Version:           m.PostedDR.Version,
+		ExecProgramID:     m.PostedDR.ExecProgramID,
+		ExecInputs:        execInputs,
+		ExecGasLimit:      m.PostedDR.ExecGasLimit,
+		TallyProgramID:    m.PostedDR.TallyProgramID,
+		TallyInputs:       tallyInputs,
+		TallyGasLimit:     m.PostedDR.TallyGasLimit,
+		ReplicationFactor: uint32(m.PostedDR.ReplicationFactor),
+		ConsensusFilter:   consensusFilter,
+		GasPrice:          gasPriceInt,
+		Memo:              memo,
+		SEDAPayload:       sedaPayload,
+		PaybackAddress:    paybackAddress,
+	}, nil
+}
+
+type CommitDataResultMsg struct {
+	DrID       string `json:"dr_id"`
+	Commitment string `json:"commitment"`
+	PublicKey  string `json:"public_key"`
+	Proof      string `json:"proof"`
+}
+
+func (m CommitDataResultMsg) EncodeToSdkMsg(sender string) (sdk.Msg, error) {
+	return &coretypes.MsgCommit{
+		Sender:    sender, // TODO ensure security
+		DrID:      m.DrID,
+		Commit:    m.Commitment,
+		PublicKey: m.PublicKey,
+		Proof:     m.Proof,
+	}, nil
+}
+
+type RevealDataResultMsg struct {
+	RevealBody coretypes.RevealBody `json:"reveal_body"`
+	PublicKey  string               `json:"public_key"`
+	Proof      string               `json:"proof"`
+	Stderr     []string             `json:"stderr"`
+	Stdout     []string             `json:"stdout"`
+}
+
+func (m RevealDataResultMsg) EncodeToSdkMsg(sender string) (sdk.Msg, error) {
+	return &coretypes.MsgReveal{
+		Sender: sender, // TODO ensure security
+		RevealBody: &coretypes.RevealBody{
+			DrID:          m.RevealBody.DrID,
+			DrBlockHeight: m.RevealBody.DrBlockHeight,
+			ExitCode:      m.RevealBody.ExitCode,
+			GasUsed:       m.RevealBody.GasUsed,
+			Reveal:        m.RevealBody.Reveal,
+			ProxyPubKeys:  m.RevealBody.ProxyPubKeys,
+		},
+		PublicKey: m.PublicKey,
+		Proof:     m.Proof,
+		Stderr:    m.Stderr,
+		Stdout:    m.Stdout,
+	}, nil
+}
