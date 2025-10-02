cd38ec587b0958873817790f2a83a9b8a02b3dd0
gluax <16431709+gluax@users.noreply.github.com>
2025-10-02 15:36:02 -0700

    feat(core): initial data request tests

--- x/core/keeper/testutil/test_account.go
+++ x/core/keeper/testutil/test_account.go
@@ -5,6 +5,8 @@ import (
 
 	"cosmossdk.io/math"
 	"github.com/cometbft/cometbft/crypto/secp256k1"
+	vrf "github.com/sedaprotocol/vrf-go"
+	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -34,4 +36,10 @@ func (ta *TestAccount) PublicKeyHex() string {
 
 func (ta *TestAccount) Balance() math.Int {
 	return ta.fixture.BankKeeper.GetBalance(ta.fixture.Context(), ta.addr, BondDenom).Amount
+}
+
+func (ta *TestAccount) Prove(hash []byte) string {
+	vrf, err := vrf.NewK256VRF().Prove(ta.signingKey.Bytes(), hash)
+	require.NoError(ta.fixture.tb, err)
+	return hex.EncodeToString(vrf)
 }
--- x/core/keeper/testutil/test_data_request.go
+++ x/core/keeper/testutil/test_data_request.go
@@ -2,10 +2,12 @@ package testutil
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"time"
 
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/exp/rand"
 
@@ -199,4 +201,121 @@ func (dr *TestDR) ExecuteDataRequestFlow(f *Fixture, numCommits, numReveals int,
 
 func (dr TestDR) GetDataRequestID() string {
 	return dr.postedID
+}
+
+func (ta *TestAccount) GetDataRequestConfig() (*types.QueryDataRequestConfigResponse, error) {
+	msg := &types.QueryDataRequestConfigRequest{}
+	return ta.fixture.CoreQuerier.DataRequestConfig(ta.fixture.Context(), msg)
+}
+
+func HashStringHelper(input string) []byte {
+	return crypto.Keccak256([]byte(input))
+}
+
+func RevealHelperFromString(input string) []byte {
+	return []byte(base64.StdEncoding.EncodeToString(HashStringHelper(input)))
+}
+
+func (ta *TestAccount) CalculateDrIdAndArgs(nonce string, replication_factor uint32) types.MsgPostDataRequest {
+	execProgramId := hex.EncodeToString(HashStringHelper(nonce))
+	execInputs := base64.StdEncoding.EncodeToString(HashStringHelper("exec_inputs"))
+	tallyProgramId := hex.EncodeToString(HashStringHelper("tally_program"))
+	tallyInputs := base64.StdEncoding.EncodeToString(HashStringHelper("tally_inputs"))
+
+	memo := base64.StdEncoding.EncodeToString(crypto.Keccak256([]byte(ta.fixture.ChainID), []byte(nonce)))
+
+	return types.MsgPostDataRequest{
+		Sender:            ta.Address(),
+		Version:           "1.0.0",
+		ExecProgramID:     execProgramId,
+		ExecInputs:        []byte(execInputs),
+		ExecGasLimit:      types.MinExecGasLimit,
+		TallyProgramID:    tallyProgramId,
+		TallyInputs:       []byte(tallyInputs),
+		TallyGasLimit:     types.MinTallyGasLimit,
+		Memo:              []byte(memo),
+		ReplicationFactor: replication_factor,
+		ConsensusFilter:   []byte(base64.StdEncoding.EncodeToString([]byte{0})),
+		GasPrice:          types.MinGasPrice,
+	}
+}
+
+func MinimumDrCost() sdk.Coin {
+	return sdk.NewCoin(BondDenom, (math.NewIntFromUint64(types.MinExecGasLimit).Add(math.NewIntFromUint64(types.MinTallyGasLimit)).Mul(types.MinGasPrice)))
+}
+
+func (ta *TestAccount) PostDataRequest(args types.MsgPostDataRequest, blockHeight int64, funds *math.Int) (*types.MsgPostDataRequestResponse, error) {
+	if blockHeight < ta.fixture.Context().BlockHeight() {
+		panic("cannot set block height to the past")
+	}
+
+	ta.fixture.Context().WithBlockHeight(blockHeight)
+
+	if funds != nil {
+		args.Funds = sdk.NewCoin(BondDenom, *funds)
+	} else {
+		args.Funds = MinimumDrCost()
+	}
+
+	return ta.fixture.CoreMsgServer.PostDataRequest(ta.fixture.Context(), &args)
+}
+
+func (ta *TestAccount) CreateRevealMsg(revealBody *types.RevealBody) *types.MsgReveal {
+	msg := &types.MsgReveal{
+		Sender:     ta.Address(),
+		RevealBody: revealBody,
+		PublicKey:  ta.PublicKeyHex(),
+		Stderr:     []string{},
+		Stdout:     []string{},
+	}
+	hash, err := msg.MsgHash(ta.fixture.ChainID)
+	require.NoError(ta.fixture.tb, err)
+	msg.Proof = ta.Prove(hash)
+	return msg
+}
+
+func (ta *TestAccount) CommitResult(revealMsg *types.MsgReveal) (*types.MsgCommitResponse, error) {
+	commitment, err := revealMsg.RevealHash()
+	require.NoError(ta.fixture.tb, err)
+
+	msg := &types.MsgCommit{
+		Sender:    ta.Address(),
+		DrID:      revealMsg.RevealBody.DrID,
+		Commit:    hex.EncodeToString(commitment),
+		PublicKey: ta.PublicKeyHex(),
+	}
+	hash, err := msg.MsgHash(ta.fixture.ChainID, int64(revealMsg.RevealBody.DrBlockHeight))
+	require.NoError(ta.fixture.tb, err)
+	msg.Proof = ta.Prove(hash)
+
+	ta.fixture.SetTx(20_000, ta.AccAddress(), msg)
+	res, err := ta.fixture.CoreMsgServer.Commit(ta.fixture.Context(), msg)
+	ta.fixture.SetInfiniteGasMeter()
+	return res, err
+}
+
+func (ta *TestAccount) RevealResult(msg *types.MsgReveal) (*types.MsgRevealResponse, error) {
+	ta.fixture.SetTx(20_000, ta.AccAddress(), msg)
+	res, err := ta.fixture.CoreMsgServer.Reveal(ta.fixture.Context(), msg)
+	ta.fixture.SetInfiniteGasMeter()
+	return res, err
+}
+
+func (ta *TestAccount) GetDataRequestsByStatus(status types.DataRequestStatus, limit uint64, lastSeenIndex *[]string) (*types.QueryDataRequestsByStatusResponse, error) {
+	msg := &types.QueryDataRequestsByStatusRequest{
+		Status: status,
+		Limit:  limit,
+	}
+	if lastSeenIndex != nil {
+		msg.LastSeenIndex = *lastSeenIndex
+	}
+
+	return ta.fixture.CoreQuerier.DataRequestsByStatus(ta.fixture.Context(), msg)
+}
+
+func (ta *TestAccount) GetDataRequest(drId string) (*types.QueryDataRequestResponse, error) {
+	msg := &types.QueryDataRequestRequest{
+		DrId: drId,
+	}
+	return ta.fixture.CoreQuerier.DataRequest(ta.fixture.Context(), msg)
 }
