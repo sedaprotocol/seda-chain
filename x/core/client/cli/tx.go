8ef8d51a34b29e6a235ad42a33810a8a3fdc0321
hacheigriega <hykim.home@gmail.com>
2025-10-16 14:58:21 -0400

    chore(x/core): add staking-related tx CLI endpoints

--- x/core/client/cli/tx.go
+++ x/core/client/cli/tx.go
@@ -17,6 +17,21 @@ import (
 	"github.com/sedaprotocol/seda-chain/x/core/types"
 )
 
+const (
+	FlagWithdrawAddress   = "withdraw-address"
+	FlagMemo              = "memo"
+	FlagVersion           = "version"
+	FlagReplicationFactor = "replication-factor"
+	FlagGasPrice          = "gas-price"
+	FlagExecProgramID     = "exec-program-id"
+	FlagExecGasLimit      = "exec-gas-limit"
+	FlagTallyProgramID    = "tally-program-id"
+	FlagTallyGasLimit     = "tally-gas-limit"
+	FlagConsensusFilter   = "consensus-filter"
+	FlagExecInputs        = "exec-inputs"
+	FlagTallyInputs       = "tally-inputs"
+)
+
 // GetTxCmd returns the CLI transaction commands for this module.
 func GetTxCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -32,6 +47,9 @@ func GetTxCmd() *cobra.Command {
 		RemoveFromAllowlist(),
 		Pause(),
 		Unpause(),
+		Stake(),
+		Unstake(),
+		Withdraw(),
 		PostDataRequest(),
 	)
 	return cmd
@@ -180,6 +198,147 @@ func Unpause() *cobra.Command {
 	return cmd
 }
 
+func Stake() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "stake [public_key_hex] [proof_hex] [stake] --memo [memo_base64] --from [staker]",
+		Short: "Stake for the given staker public key",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			// Public key and proof must be valid hex strings.
+			publicKey := args[0]
+			proof := args[1]
+
+			_, err = hex.DecodeString(publicKey)
+			if err != nil {
+				return err
+			}
+			_, err = hex.DecodeString(proof)
+			if err != nil {
+				return err
+			}
+
+			// Memo, if provided, must be a valid base64 string.
+			memo, err := cmd.Flags().GetString("memo")
+			if err != nil {
+				return err
+			}
+			if memo != "" {
+				_, err = base64.StdEncoding.DecodeString(memo)
+				if err != nil {
+					return err
+				}
+			}
+
+			// Stake must be a valid coin.
+			stake, err := sdk.ParseCoinNormalized(args[2])
+			if err != nil {
+				return err
+			}
+
+			msg := &types.MsgStake{
+				Sender:    clientCtx.GetFromAddress().String(),
+				PublicKey: publicKey,
+				Memo:      memo,
+				Proof:     proof,
+				Stake:     stake,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	cmd.Flags().StringP("memo", "m", "", "optional memo for staking")
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func Unstake() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "unstake [public_key_hex] [proof_hex] --from [staker]",
+		Short: "Unstake all staked tokens for the given staker public key",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			// Public key and proof must be valid hex strings.
+			publicKey := args[0]
+			proof := args[1]
+
+			_, err = hex.DecodeString(publicKey)
+			if err != nil {
+				return err
+			}
+			_, err = hex.DecodeString(proof)
+			if err != nil {
+				return err
+			}
+
+			msg := &types.MsgUnstake{
+				Sender:    clientCtx.GetFromAddress().String(),
+				PublicKey: publicKey,
+				Proof:     proof,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+func Withdraw() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "withdraw [public_key_hex] [proof_hex] --from [staker]",
+		Short: "Withdraw rewards for the given staker public key",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			publicKey := args[0]
+			proof := args[1]
+
+			_, err = hex.DecodeString(publicKey)
+			if err != nil {
+				return err
+			}
+			_, err = hex.DecodeString(proof)
+			if err != nil {
+				return err
+			}
+
+			withdrawAddress, err := cmd.Flags().GetString(FlagWithdrawAddress)
+			if err != nil {
+				return err
+			}
+			if withdrawAddress == "" {
+				withdrawAddress = clientCtx.GetFromAddress().String()
+			}
+
+			msg := &types.MsgWithdraw{
+				Sender:          clientCtx.GetFromAddress().String(),
+				PublicKey:       publicKey,
+				Proof:           proof,
+				WithdrawAddress: withdrawAddress,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	cmd.Flags().String(FlagWithdrawAddress, "", "optional withdraw address (defaults to tx sender)")
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
 func PostDataRequest() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "post-data-request [public_key] [funds] --from [requester_address]",
@@ -214,26 +373,26 @@ func PostDataRequest() *cobra.Command {
 				Funds:  funds[0],
 			}
 
-			if rf, err := cmd.Flags().GetUint32("replication-factor"); err == nil {
+			if rf, err := cmd.Flags().GetUint32(FlagReplicationFactor); err == nil {
 				if rf == 0 {
 					return fmt.Errorf("replication factor must be greater than 0")
 				}
 				msg.ReplicationFactor = rf
 			}
 
-			if version, err := cmd.Flags().GetString("version"); err == nil {
+			if version, err := cmd.Flags().GetString(FlagVersion); err == nil {
 				if version != "" {
 					msg.Version = version
 				}
 			}
 
-			if memo, err := cmd.Flags().GetString("memo"); err == nil {
+			if memo, err := cmd.Flags().GetString(FlagMemo); err == nil {
 				if memo != "" {
 					msg.Memo = []byte(base64.StdEncoding.EncodeToString([]byte(memo)))
 				}
 			}
 
-			execID, err := cmd.Flags().GetString("exec-program-id")
+			execID, err := cmd.Flags().GetString(FlagExecProgramID)
 			if err != nil {
 				return err
 			}
@@ -242,7 +401,7 @@ func PostDataRequest() *cobra.Command {
 			}
 			msg.ExecProgramID = execID
 
-			execInputs, err := cmd.Flags().GetString("exec-inputs")
+			execInputs, err := cmd.Flags().GetString(FlagExecInputs)
 			if err != nil {
 				return err
 			}
@@ -251,7 +410,7 @@ func PostDataRequest() *cobra.Command {
 			}
 			msg.ExecInputs = []byte(execInputs)
 
-			tallyID, err := cmd.Flags().GetString("tally-program-id")
+			tallyID, err := cmd.Flags().GetString(FlagTallyProgramID)
 			if err != nil {
 				return err
 			}
@@ -261,7 +420,7 @@ func PostDataRequest() *cobra.Command {
 				msg.TallyProgramID = tallyID
 			}
 
-			tallyInputs, err := cmd.Flags().GetString("tally-inputs")
+			tallyInputs, err := cmd.Flags().GetString(FlagTallyInputs)
 			if err != nil {
 				return err
 			}
@@ -270,7 +429,7 @@ func PostDataRequest() *cobra.Command {
 			}
 			msg.TallyInputs = []byte(tallyInputs)
 
-			gasPriceString, err := cmd.Flags().GetString("gas-price")
+			gasPriceString, err := cmd.Flags().GetString(FlagGasPrice)
 			if err != nil {
 				return err
 			}
@@ -283,19 +442,19 @@ func PostDataRequest() *cobra.Command {
 			}
 			msg.GasPrice = gasPrice
 
-			execGasLimit, err := cmd.Flags().GetUint64("exec-gas-limit")
+			execGasLimit, err := cmd.Flags().GetUint64(FlagExecGasLimit)
 			if err != nil {
 				return err
 			}
 			msg.ExecGasLimit = execGasLimit
 
-			tallyGasLimit, err := cmd.Flags().GetUint64("tally-gas-limit")
+			tallyGasLimit, err := cmd.Flags().GetUint64(FlagTallyGasLimit)
 			if err != nil {
 				return err
 			}
 			msg.TallyGasLimit = tallyGasLimit
 
-			consensusFilter, err := cmd.Flags().GetString("consensus-filter")
+			consensusFilter, err := cmd.Flags().GetString(FlagConsensusFilter)
 			if err != nil {
 				return err
 			}
@@ -309,17 +468,17 @@ func PostDataRequest() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringP("memo", "m", "", "optional memo for the data request")
-	cmd.Flags().StringP("version", "v", "v1.0.0", "version for the data request")
-	cmd.Flags().Uint32P("replication-factor", "r", 1, "replication factor for the data request")
-	cmd.Flags().String("gas-price", "2000", "gas price for the data request")
-	cmd.Flags().StringP("exec-program-id", "e", "", "execution program ID for the data request in ")
-	cmd.Flags().Uint64("exec-gas-limit", 300_000_000_000_000, "execution gas limit for the data request")
-	cmd.Flags().StringP("tally-program-id", "t", "", "tally program ID for the data request")
-	cmd.Flags().Uint64("tally-gas-limit", 300_000_000_000_000, "tally gas limit for the data request")
-	cmd.Flags().String("exec-inputs", "", "execution inputs for the data request encoded in hex")
-	cmd.Flags().String("tally-inputs", "", "tally inputs for the data request encoded in hex")
-	cmd.Flags().StringP("consensus-filter", "", "", "optional consensus filter for the data request encoded in hex")
+	cmd.Flags().StringP(FlagMemo, "m", "", "optional memo for the data request")
+	cmd.Flags().StringP(FlagVersion, "v", "v1.0.0", "version for the data request")
+	cmd.Flags().Uint32P(FlagReplicationFactor, "r", 1, "replication factor for the data request")
+	cmd.Flags().String(FlagGasPrice, "2000", "gas price for the data request")
+	cmd.Flags().StringP(FlagExecProgramID, "e", "", "execution program ID for the data request in hex")
+	cmd.Flags().Uint64(FlagExecGasLimit, 300_000_000_000_000, "execution gas limit for the data request")
+	cmd.Flags().StringP(FlagTallyProgramID, "t", "", "tally program ID for the data request")
+	cmd.Flags().Uint64(FlagTallyGasLimit, 300_000_000_000_000, "tally gas limit for the data request")
+	cmd.Flags().String(FlagExecInputs, "", "execution inputs for the data request encoded in hex")
+	cmd.Flags().String(FlagTallyInputs, "", "tally inputs for the data request encoded in hex")
+	cmd.Flags().StringP(FlagConsensusFilter, "", "", "optional consensus filter for the data request encoded in hex")
 	flags.AddTxFlagsToCmd(cmd)
 	return cmd
 }
