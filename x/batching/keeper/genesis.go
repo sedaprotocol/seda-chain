25fab2802bf9d009334de97594eae1907bbb638c
Hyoung-yoon Kim <hykim.home@gmail.com>
2025-11-24 11:00:19 -0500

    fix(x/batching): fix omission of batched status in InitGenesis

--- x/batching/keeper/genesis.go
+++ x/batching/keeper/genesis.go
@@ -10,36 +10,43 @@ import (
 
 // InitGenesis puts all data from genesis state into store.
 func (k Keeper) InitGenesis(ctx sdk.Context, data types.GenesisState) {
-	if err := k.setCurrentBatchNum(ctx, data.CurrentBatchNumber); err != nil {
+	err := k.setCurrentBatchNum(ctx, data.CurrentBatchNumber)
+	if err != nil {
 		panic(err)
 	}
 	for _, batch := range data.Batches {
-		if err := k.setBatch(ctx, batch); err != nil {
+		err := k.setBatch(ctx, batch)
+		if err != nil {
 			panic(err)
 		}
 	}
 	for _, data := range data.BatchData {
-		if err := k.setDataResultTreeEntry(ctx, data.BatchNumber, data.DataResultEntries); err != nil {
+		err := k.setDataResultTreeEntry(ctx, data.BatchNumber, data.DataResultEntries)
+		if err != nil {
 			panic(err)
 		}
 		for _, valEntry := range data.ValidatorEntries {
-			if err := k.setValidatorTreeEntry(ctx, data.BatchNumber, valEntry); err != nil {
+			err := k.setValidatorTreeEntry(ctx, data.BatchNumber, valEntry)
+			if err != nil {
 				panic(err)
 			}
 		}
 		for _, sig := range data.BatchSignatures {
-			if err := k.SetBatchSigSecp256k1(ctx, data.BatchNumber, sig.ValidatorAddress, sig.Secp256K1Signature); err != nil {
+			err := k.SetBatchSigSecp256k1(ctx, data.BatchNumber, sig.ValidatorAddress, sig.Secp256K1Signature)
+			if err != nil {
 				panic(err)
 			}
 		}
 	}
-	for _, dataResult := range data.DataResults {
-		if err := k.dataResults.Set(ctx, collections.Join3(false, dataResult.DataResult.DrId, dataResult.DataResult.DrBlockHeight), dataResult.DataResult); err != nil {
+	for _, dr := range data.DataResults {
+		err := k.dataResults.Set(ctx, collections.Join3(dr.Batched, dr.DataResult.DrId, dr.DataResult.DrBlockHeight), dr.DataResult)
+		if err != nil {
 			panic(err)
 		}
 	}
 	for _, batchAssignment := range data.BatchAssignments {
-		if err := k.SetBatchAssignment(ctx, batchAssignment.DataRequestId, batchAssignment.DataRequestHeight, batchAssignment.BatchNumber); err != nil {
+		err := k.SetBatchAssignment(ctx, batchAssignment.DataRequestId, batchAssignment.DataRequestHeight, batchAssignment.BatchNumber)
+		if err != nil {
 			panic(err)
 		}
 	}
